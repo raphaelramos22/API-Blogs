238d3b69894e48e97b51d3296b8e84bb7a6fa6eb
Raphael <rafa.ramos_22@hotmail.com>
2022-07-16 13:25:58 -0300

    feat: adicionanroute do endpoint get/user que traz todos os usuarios

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/userController');
 const routerUser = Router();
 
 routerUser.post('/', userController.userCreate);
+routerUser.get('/', userController.userAll);
 
 module.exports = routerUser;
