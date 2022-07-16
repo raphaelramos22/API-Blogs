381305cb1f09627365b7829187fe31fb5dda7871
Raphael <rafa.ramos_22@hotmail.com>
2022-07-15 23:07:24 -0300

    feat: adicionando userServices para as validaçoes para criar usuario

---
+++ src/services/userServices.js
@@ -0,0 +1,38 @@
+const Joi = require('joi');
+const { User } = require('../database/models');
+const JWT = require('./jwt.services');
+
+const schema = Joi.object({
+  displayName: Joi.string().min(8).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  image: Joi.string().required(),
+});
+
+const userCreate = async (displayName, email, password, image) => {
+  const { error } = schema.validate({ displayName, email, password, image });
+
+  if (error) {
+    const e = new Error(error.message);
+    e.code = 'ValidationError';
+    throw e;
+  }
+
+  const userExist = await User.findOne({ where: { email } });
+
+  if (userExist) {
+    const e = new Error('User already registered');
+    e.code = 'ConflictError';
+    throw e;
+  }
+
+  await User.create({ displayName, email, password, image });
+
+  const token = JWT.createToken(password);
+
+  return token;
+};
+
+module.exports = {
+  userCreate,
+};
