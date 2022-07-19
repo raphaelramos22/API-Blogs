c4719bde8cb272180a2dfd1d27d5da7b0151e264
Raphael <rafa.ramos_22@hotmail.com>
2022-07-19 14:40:39 -0300

    feat: adicionando funçao categoryAll no cetegoryController

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -9,7 +9,16 @@ const categoryCreate = async (req, res, next) => {
     next(error);
   }
 };
+const categoryAll = async (_req, res, next) => {
+  try {
+    const result = await categoryService.categoryAll();
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   categoryCreate,
+  categoryAll,
 };
