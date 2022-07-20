1015f7933664c6583d21d2dd6830b0f7c4d4e324
Raphael <rafa.ramos_22@hotmail.com>
2022-07-20 14:00:14 -0300

    feat: adicionando funçao postsAll no postCotroller que faz o colotrole para trazer todos os bogs post, user dono dele e as categorias

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -10,7 +10,16 @@ const postCreate = async (req, res, next) => {
     next(err);
   }
 };
+const postsAll = async (_req, res, next) => {
+  try {
+    const posts = await postServices.postsAll();
+    return res.status(200).json(posts);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   postCreate,
+  postsAll,
 };
